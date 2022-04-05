4e3a2c6e1157eca62deba14a98a6f0c78022522f
EvilTerabite <48357426+EvilTerabite@users.noreply.github.com>
2022-04-05 02:38:09 -0400

    switched to spring boot

---
+++ src/main/java/ca/tcemap/tcetrains/TceTrainsApplication.java
@@ -0,0 +1,13 @@
+package ca.tcemap.tcetrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TceTrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TceTrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/tcemap/tcetrains/TceTrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tcemap.tcetrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TceTrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
